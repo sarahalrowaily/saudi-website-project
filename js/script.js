cc86a380dfb465a62aa74badd5fc6389e5d4edbe
Sara <443200756@student.ksu.edu.sa>
2026-04-24 05:04:59 +0300

    Add filterRegions function to filter cards by type

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 function toggleMode() {
     document.body.classList.toggle("dark-mode");
 }
+function filterRegions(type) {
+    var cards = document.querySelectorAll(".card");
+
+    cards.forEach(function(card) {
+        if (type === "all") {
+            card.style.display = "block";
+        } else if (card.classList.contains(type)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
+}
